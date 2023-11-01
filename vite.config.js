340ce6788ab0f5115de45aef41761e61d7e203b3
rajat arora <rajatarora@rajats-MacBook-Air.local>
2023-11-02 01:45:39 +0530

    backend connection

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import { config } from "dotenv"; // Import the 'config' method from 'dotenv'
+
+// // Load environment variables from the .env file
+// config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
