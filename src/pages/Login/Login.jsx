85329c261e76cbd8d23ddef7b74f3e9ec4bfd5f1
rajat arora <rajatarora@rajats-MacBook-Air.local>
2023-11-11 00:29:18 +0530

    user sign in

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("password");
+
+  const handlePasswordShow = () => {
+    if (type == "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  };
+
+  const handleUserSignIn = async () => {
+    // check of user has something in redux store for cart or wish list
+    // if yes make the object accordingly
+
+    const signInObject = {
+      emailId: emailId,
+      password: password,
+    };
+
+    const signInData = await axios.post("http://localhost:8080/food-villa/api/v1/sign-in", signInObject);
+
+    console.log(signInData);
+  };
+
   return (
     <div className='mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8'>
       <div className='mx-auto max-w-lg'>
@@ -11,7 +39,7 @@ const Login = () => {
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sunt dolores deleniti inventore quaerat mollitia?
         </p>
 
-        <form action='' className='mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8'>
+        <div className='mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8'>
           <p className='text-center text-lg font-medium'>Sign into your account</p>
 
           <div>
@@ -20,7 +48,13 @@ const Login = () => {
             </label>
 
             <div className='relative'>
-              <input type='email' className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm' placeholder='Enter email' />
+              <input
+                type='email'
+                onChange={(e) => setEmailId(e.target.value)}
+                className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                placeholder='Enter email'
+                value={emailId}
+              />
 
               <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
                 <svg xmlns='http://www.w3.org/2000/svg' className='h-4 w-4 text-gray-400' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
@@ -41,10 +75,23 @@ const Login = () => {
             </label>
 
             <div className='relative'>
-              <input type='password' className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm' placeholder='Enter password' />
+              <input
+                type={type}
+                onChange={(e) => setPassword(e.target.value)}
+                className='w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm'
+                placeholder='Enter password'
+                value={password}
+              />
 
               <span className='absolute inset-y-0 end-0 grid place-content-center px-4'>
-                <svg xmlns='http://www.w3.org/2000/svg' className='h-4 w-4 text-gray-400' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-4 w-4 text-gray-400 cursor-pointer'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  onClick={handlePasswordShow}
+                >
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />
                   <path
                     strokeLinecap='round'
@@ -57,7 +104,7 @@ const Login = () => {
             </div>
           </div>
 
-          <button type='submit' className='block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white cursor-pointer'>
+          <button className='block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white cursor-pointer' onClick={handleUserSignIn}>
             Sign in
           </button>
 
@@ -78,7 +125,7 @@ const Login = () => {
               Take me home
             </span>
           </p>
-        </form>
+        </div>
       </div>
     </div>
   );
