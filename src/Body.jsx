7779bb5b3c80f7de252b10f1eea589782162fb66
rajat arora <rajatarora@rajats-MacBook-Air.local>
2023-11-14 11:11:25 +0530

    kind of added persist

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,14 +3,15 @@ import HomePage from "./pages/HomePage/HomePage";
 import About from "./pages/About/About";
 import ShopPage from "./pages/CommonShopPage/ShopPage";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+// import { store } from "./redux/store";
 import ErrorPage from "./pages/Error/ErrorPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+// import { persistStore } from "redux-persist";
+import { store, persistedStore } from "./redux/store";
 
 const Body = () => {
   const appRouter = createBrowserRouter([
@@ -29,9 +30,9 @@ const Body = () => {
   return (
     <div>
       <Provider store={store}>
-        {/* <PersistGate loading={<h1 className='text-xs text-white'> LOADING</h1>} persistor={persistor}> */}
-        <RouterProvider router={appRouter} />
-        {/* </PersistGate> */}
+        <PersistGate loading={<h1 className='text-xs text-white'> LOADING</h1>} persistor={persistedStore}>
+          <RouterProvider router={appRouter} />
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,9 @@ import pageSlice from "./Slices/routeSlics";
 import wishListSlice from "./Slices/wishListSlice";
 import cartSlice from "./Slices/cartSlice";
 
-// import { persistReducer } from "redux-persist";
-// import { combineReducers } from "@reduxjs/toolkit";
-// import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 
 // const persistConfig = {
 //   key: "root",
@@ -13,6 +13,24 @@ import cartSlice from "./Slices/cartSlice";
 //   storage,
 // };
 
+// const persistWishListReducer = persistReducer(persistConfig, wishListSlice);
+// const persistCartReducer = persistReducer(persistConfig, cartSlice);
+
+// const reducer = combineReducers({
+//   wishList: persistWishListReducer,
+//   cartList: persistCartReducer,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, reducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+// });
+
+// export const persistedStore = persistStore(store);
+
+////////////////////////////////////////////////////////
+// Without Persist
 // const reducer = combineReducers({
 //   currentPage: pageSlice,
 //   wishList: wishListSlice,
@@ -23,9 +41,78 @@ import cartSlice from "./Slices/cartSlice";
 // const store = configureStore({
 //   reducer: persistedReducer,
 // });
+///////////////////////////////////////////////////////////]
 
-const store = configureStore({
-  reducer: { currentPage: pageSlice, wishList: wishListSlice, cartList: cartSlice },
+// workign without persist
+// export const store = configureStore({
+//   reducer: { currentPage: pageSlice, wishList: wishListSlice, cartList: cartSlice },
+// });
+
+// export default store;
+
+///////////////////////////////////////////////////////
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers } from "redux";
+// import { persistReducer, persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// // import pageSlice from "./Slices/routeSlics";
+// import wishListSlice from "./Slices/wishListSlice";
+// import cartSlice from "./Slices/cartSlice";
+
+// const persistConfig = {
+//   key: "wishlist",
+//   version: 1,
+//   storage,
+// };
+
+// const persistConfig2 = {
+//   key: "cart",
+//   version: 1,
+//   storage,
+// };
+
+// const reducer = combineReducers({
+//   // currentPage: pageSlice,
+//   wishList: persistReducer(persistConfig, wishListSlice),
+//   cartList: persistReducer(persistConfig2, cartSlice),
+// });
+
+// const persistedReducer = persistReducer(persistConfig, reducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+// });
+
+// export const persistedStore = persistStore(store);
+
+const rootPersistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistConfigWishlist = {
+  key: "wishList",
+  version: 1,
+  storage,
+};
+
+const persistConfigCartlist = {
+  key: "cartList",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  wishList: persistReducer(persistConfigWishlist, wishListSlice),
+  cartList: persistReducer(persistConfigCartlist, cartSlice),
+  currentPage: pageSlice,
 });
 
-export default store;
+const persistedReducer = persistReducer(rootPersistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistedStore = persistStore(store);
